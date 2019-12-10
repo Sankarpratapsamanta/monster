712ef914bee3f56f79b689c933ec8ed0883082d7
unknown <sankarpratapsamanta56@gmail.com>
2019-12-10 20:42:29 +0530

    Updates

---
+++ precache-manifest.8a6de318c449eaeae0b2276ba720bf62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83e0abd4482fab4a3e21505b47d5917",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "be3dacb6b29c841599fd",
+    "url": "/monster/static/css/main.1519c216.chunk.css"
+  },
+  {
+    "revision": "d935b8e3403269409eb0",
+    "url": "/monster/static/js/2.722cd26d.chunk.js"
+  },
+  {
+    "revision": "be3dacb6b29c841599fd",
+    "url": "/monster/static/js/main.d84cf671.chunk.js"
+  },
+  {
+    "revision": "244b35acf1b766a6eb91",
+    "url": "/monster/static/js/runtime-main.7280ffcc.js"
+  }
+]);
